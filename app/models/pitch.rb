ea4b0900d639c9a26cc489af15de744167f25652
dinhthi12 <thi884641@gmail.com>
2023-07-06 10:24:51 +0700

    screen booking pitch

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -3,7 +3,6 @@ class Pitch < ApplicationRecord
 
   has_many :pitch_bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  accepts_nested_attributes_for :pitch_booking_services, allow_destroy: true
 
   validates :name, presence: true
   validates :image, content_type: {in: %w(image/jpeg image/gif image/png),
--- app/models/pitch_booking.rb
+++ app/models/pitch_booking.rb
@@ -3,4 +3,5 @@ class PitchBooking < ApplicationRecord
   belongs_to :pitch
   has_many :pitch_booking_services, dependent: :destroy
   has_many :services, through: :pitch_booking_services
+  accepts_nested_attributes_for :pitch_booking_services, allow_destroy: true
 end
